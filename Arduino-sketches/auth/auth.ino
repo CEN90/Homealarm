1a22f94b174b51f152fea7e2442d9470240e2fe1
CEN90 <christopher.nordin@outlook.com>
2023-11-22 13:32:57 +0100

    Experiment on reading from controller

--- Arduino-sketches/auth/auth.ino
+++ Arduino-sketches/auth/auth.ino
@@ -1,14 +1,25 @@
-int pin = PIN1;
+constexpr uint8_t pin_len = 5;
+constexpr uint8_t pin_input[pin_len] = { 9, 10, 11, 12, 13 };
+constexpr uint8_t pin_led[pin_len] = { 3, 4, 5, 6, 7 };
+
+constexpr uint8_t sleep = 1;
 
 void setup() {
-    pinMode(pin, OUTPUT);
+    for (size_t i = 0; i < pin_len; i++)
+    {
+        pinMode(pin_input[i], INPUT);
+        pinMode(pin_led[i], OUTPUT);
+    }
+    
 }
 
 void loop() {
-    digitalWrite(pin, HIGH);
-    delay(1000);
-    digitalWrite(pin, LOW);
-    delay(1000);
+    for (size_t i = 0; i < pin_len; i++)
+    {
+        digitalWrite(pin_led[i], digitalRead(pin_input[i]));
+    }
+    
+    delay(sleep);
 }
 
 // alarm -> Röd
