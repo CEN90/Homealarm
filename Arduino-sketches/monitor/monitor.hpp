1f7a975c3206686f1137db74d478bf56dcf68e13
CEN90 <christopher.nordin@outlook.com>
2023-11-23 09:47:51 +0100

    Inte Kermit arm

--- Arduino-sketches/monitor/monitor.hpp
+++ Arduino-sketches/monitor/monitor.hpp
@@ -1,12 +1,16 @@
 #include "EdgeReader.hpp"
 
-constexpr uint8_t ALARM_INPUT = 9;
-constexpr uint8_t ARMED_INPUT = 10;
-constexpr uint8_t DOOR_LOCK_INPUT = 11;
-constexpr uint8_t KEY_VALID_INPUT = 12;
-constexpr uint8_t DOOR_SENSOR_INPUT = 13;
+constexpr uint8_t ALARM_IN = 9;
+constexpr uint8_t ARMED_IN = 10;
+constexpr uint8_t DOOR_LOCK_IN = 11;
+constexpr uint8_t KEY_VALID_IN = 12;
+constexpr uint8_t DOOR_SENSOR_IN = 13;
 
-constexpr uint8_t 
+constexpr uint8_t ALARM_OUT = PIN3;
+constexpr uint8_t ARMED_OUT = PIN4;
+constexpr uint8_t DOOR_LOCK_OUT = PIN5;
+constexpr uint8_t KEY_VALID_OUT = PIN6;
+constexpr uint8_t DOOR_SENSOR_OUT = PIN7;
 
 constexpr uint8_t ERROR_STATE = -1;
 constexpr uint8_t POLL_TIME = 10;
